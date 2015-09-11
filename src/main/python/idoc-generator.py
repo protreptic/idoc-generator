937f4ede0d04d975bbb21711fbd98836624498f2
bukhal <bukhal@i-teco.ru>
2015-09-11 17:06:20 +0300

    Script args support added

--- src/main/python/idoc-generator.py
+++ src/main/python/idoc-generator.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
+from collections import defaultdict
 
 __author__ = 'peterbukhal'
 
 import xml.etree.ElementTree as ET
 import os
 import uuid
+import argparse
 
 output_directory = 'output'
 
-if (not os.path.isdir(output_directory)):
-    os.mkdir(output_directory)
-
-def generate_idoc(idoc_num):
-    tree = ET.parse('../resources/delivery_template.xml')
+def generate_idoc():
+    tree = ET.parse(template_file)
     root = tree.getroot()
 
     for bolnr_tag in root.iter('BOLNR'):
@@ -26,5 +25,31 @@ def generate_idoc(idoc_num):
 
     tree.write(output_directory + str('/idoc-' + str(uuid.uuid4()) + '.xml').upper())
 
-for idoc_number in range(1, 101):
-    generate_idoc(idoc_number)
+parser = argparse.ArgumentParser(prog='idoc-generator')
+parser.add_argument("--output",
+                    type=str, help="A full path to output folder for idocs")
+parser.add_argument("--template",
+                    type=str, help="A full path to an IDOC template file")
+parser.add_argument("--gen-number",
+                    type=int, help="Number IDOCs to be generated")
+parser.add_argument("--clean",
+                    type=bool,
+                    help="")
+
+args = parser.parse_args()
+
+print args.output;
+
+output_directory = args.output
+clean_directory = args.clean
+template_file = args.template
+gen_number = args.gen_number
+
+if (not os.path.isdir(output_directory)):
+    os.mkdir(output_directory)
+
+if (clean_directory):
+    for idoc in os.listdir(output_directory):
+        os.remove(output_directory + '/' + idoc)
+
+for idoc_number in range(0, gen_number): generate_idoc()
