50cec484943e77a0c9562864f7217123b51a2bde
bukhal <bukhal@i-teco.ru>
2015-09-11 15:56:48 +0300

    new approach in uuid generation for IDOCs

--- src/main/python/idoc-generator.py
+++ src/main/python/idoc-generator.py
@@ -4,37 +4,28 @@ __author__ = 'peterbukhal'
 
 import xml.etree.ElementTree as ET
 import os
+import uuid
 
 output_directory = 'output'
 
 if (not os.path.isdir(output_directory)):
     os.mkdir(output_directory)
 
-def generate_idoc(idoc_num, bolnr_num, xabln_num, sscc_num):
+def generate_idoc(idoc_num):
     tree = ET.parse('../resources/delivery_template.xml')
     root = tree.getroot()
 
-    for bolnr in root.iter('BOLNR'):
-        bolnr.text = str(bolnr_num)
+    for bolnr_tag in root.iter('BOLNR'):
+        bolnr_tag.text = str(uuid.uuid4().int)[0:10]
 
-    for xabln in root.iter('XABLN'):
-        xabln.text = str(xabln_num)
+    for xabln_tag in root.iter('XABLN'):
+        xabln_tag.text = str(uuid.uuid4().int)[0:10]
 
-    for sscc in root.iter('SSCC'):
-        sscc.text = str(sscc_num)
+    for sscc_tag in root.iter('SSCC'):
+        sscc_tag.text = str(uuid.uuid4().int)[0:18]
 
-    print ('bolnr: ' + str(bolnr_num) + ' xabln: ' + str(xabln_num) + ' sscc: ' + str(sscc_num) + ' -> ' + output_directory + '/idoc' + str(idoc_num) + '.xml')
+    tree.write(output_directory + '/idoc' + str(idoc_num) + '.xml')
 
-    tree.write(output_directory + '/idoc' + str(idoc_number) + '.xml')
-
-bolnr = 121662240
-xabln = 123522790
-sscc = 789613220008442413
-
-for idoc_number in range(1, 21):
-    bolnr += 1
-    xabln += 1
-    sscc += 1
-
-    generate_idoc(idoc_number, bolnr, xabln, sscc)
+for idoc_number in range(1, 101):
+    generate_idoc(idoc_number)
 
